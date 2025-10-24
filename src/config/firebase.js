23c91149bce6d7a3fb43a9e883149e3fd58cd783
lnu8926-web <lnu8926@gmail.com>
2025-10-24 13:56:01 +0900

    로컬 변경사항 임시 커밋

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
-import { getStorage } from "firebase/storage"; // * 추가
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyCskvJkXOKFoGavU8__DUelOtw8Bv9ziBY",
+  authDomain: "pokemonproject-672a3.firebaseapp.com",
+  projectId: "pokemonproject-672a3",
+  storageBucket: "pokemonproject-672a3.firebasestorage.app",
+  messagingSenderId: "1088796164839",
+  appId: "1:1088796164839:web:c9c4412f65d10d71e15842",
+  measurementId: "G-28FM4HJXLM",
 };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app); 
-export const storage = getStorage(app); // * 추가
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
